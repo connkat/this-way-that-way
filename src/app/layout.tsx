5640db21e3b2b96e1097c969408339be1693d42a
Kat <hi.connkat@gmail.com>
2025-07-27 16:57:27 -0600

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { gtUltraFine, gtUltraMedian, gtUltraStandard } from "@/styles/fonts";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${gtUltraFine.variable} ${gtUltraMedian.variable} ${gtUltraStandard.variable} antialiased`}
       >
         {children}
       </body>
