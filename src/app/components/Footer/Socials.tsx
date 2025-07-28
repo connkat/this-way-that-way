0a17e8edf5934ef376f1b97aa256f276a209da3a
Kat <hi.connkat@gmail.com>
2025-07-27 18:28:25 -0600

    icons

--- src/app/components/Footer/Socials.tsx
+++ src/app/components/Footer/Socials.tsx
@@ -1,25 +1,27 @@
+import { FaXTwitter, FaInstagram, FaLinkedin } from "react-icons/fa6";
+
 export default function Socials() {
   return (
     <div>
       <h3 className="font-standard-bold text-xl mb-4">Follow Us</h3>
-      <div className="space-y-2">
+      <div className="flex flex-row space-x-4">
         <a
           href="#"
-          className="font-standard-regular block transition-colors hover:text-[#F8F269]"
+          className="font-standard-regular flex items-center gap-2 transition-colors hover:text-[#F8F269]"
         >
-          Twitter
+          <FaXTwitter size={20} />
         </a>
         <a
           href="#"
-          className="font-standard-regular block transition-colors hover:text-[#F8F269]"
+          className="font-standard-regular flex items-center gap-2 transition-colors hover:text-[#F8F269]"
         >
-          Instagram
+          <FaInstagram size={20} />
         </a>
         <a
           href="#"
-          className="font-standard-regular block transition-colors hover:text-[#F8F269]"
+          className="font-standard-regular flex items-center gap-2 transition-colors hover:text-[#F8F269]"
         >
-          LinkedIn
+          <FaLinkedin size={20} />
         </a>
       </div>
     </div>
