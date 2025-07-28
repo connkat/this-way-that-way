a1a793afd325371aa15a8e72eb4012a629f27a52
Kat <hi.connkat@gmail.com>
2025-07-27 18:42:41 -0600

    location

--- src/app/components/Footer/Location.tsx
+++ src/app/components/Footer/Location.tsx
@@ -1,10 +1,18 @@
 export default function Location() {
   return (
     <div>
-      <h3 className="font-standard-bold text-xl mb-4">Location</h3>
-      <p className="font-standard-regular mb-2">House 831</p>
-      <p className="font-standard-regular mb-2">831 17 Ave SW</p>
-      <p className="font-standard-regular mb-2">Calgary, Alberta</p>
+      <h3 className="font-standard-bold text-xl mb-2">Location</h3>
+      <a
+        href="https://maps.app.goo.gl/ecw6S1paCtS6nCVs5"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p className="font-standard-regular mb-2">
+          House 831 <br />
+          831 17 Ave SW <br />
+          Calgary, Alberta
+        </p>
+      </a>
     </div>
   );
 }
